fc2dd66c5a3c77f4b0edc0340a0a05229194ab77
hans chan <sugar77_chan@icloud.com>
2025-07-31 20:48:50 +0800

    增加了新增机器的剧本

---
+++ ReadME.md
@@ -0,0 +1,21 @@
+## 项目简介
+本项目的主要作用是通过ansible调用conical公司旗下maas的api，从而对裸金属进行管理。本项目在maas3.5和maas3.6中测试通过。
+
+## 使用简介
+### 1.安装和配置相关依赖
+本项目需登录maas管理平台获取api_key，在groups_vars/all.yaml文件中将maas_url和api_key变量修改为相应的值。
+```shell
+pip3 install -r requirements.txt
+```
+
+### 2.向maas中添加机器
+```shell
+ansible-playbook playbooks/add_machines.yml -e "hostname=client02 mac_address=00:0c:29:4a:cb:ab ip_address=192.168.157.36"
+```
+执行如上命令可以将机器裸金属添加到maas平台，并且将设置的ip和mac地址做静态dhcp绑定，因此在后续使用过程中机器状态发生变化也能分配相同的ip地址，确保机器全生命周期内ip、主机名、mac绑定关系的唯一性。
+
+playbook添加机器的电源模式为manual，可以在使用时候根据需求改成相应的模式，并增加相应的模式的参数。默认机器添加后所在的zone为local_zone、pool为，bare，可以根据需求修改变量
+**前置条件**
+a.在执行添加机器之前需配置好机器的ipmi、virsh、pve等相关信息；
+b.机器只有一张网卡启用了pxe功能；
+c、预先知道启用了pxe网卡的mac地址，预先规划了机器的主机名；
---
+++ inventory.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+import sys
+import os
+import json
+import yaml
+from requests_oauthlib import OAuth1Session
+
+# 自动查找 all.yaml 的路径（优先 group_vars，再 vars）
+def load_config():
+    base_dir = os.path.dirname(__file__)
+    for rel_path in ['group_vars/all.yaml', 'vars/all.yaml']:
+        full_path = os.path.join(base_dir, rel_path)
+        if os.path.exists(full_path):
+            with open(full_path, 'r') as f:
+                return yaml.safe_load(f)
+    raise FileNotFoundError("未找到 group_vars/all.yaml 或 vars/all.yaml")
+
+# 加载配置
+config = load_config()
+MAAS_URL = config['maas_url']
+API_KEY = config['api_key']
+
+# 拆分 API KEY
+client_key, token_key, token_secret = API_KEY.split(':')
+session = OAuth1Session(
+    client_key=client_key,
+    resource_owner_key=token_key,
+    resource_owner_secret=token_secret
+)
+
+def get_machines(resource_type):
+    url = f"{MAAS_URL}api/2.0/{resource_type}/"
+    resp = session.get(url)
+    resp.raise_for_status()
+    return resp.json()
+
+def get_os_mapping():
+    return {
+        "8": "centos8",
+        "7": "centos7",
+        "bionic": "ubuntu18",
+        "focal": "ubuntu20",
+        "jammy": "ubuntu22",
+        "mantic": "ubuntu23",
+        "noble": "ubuntu24"
+    }
+
+def build_inventory(machines):
+    os_mapping = get_os_mapping()
+    inventory = {
+        '_meta': {
+            'hostvars': {}
+        }
+    }
+
+    for machine in machines:
+        if machine['node_type_name'] == "Rack controller":
+            ansible_user = "hans"
+            group_name = "rack_controllers"
+        elif machine['node_type_name'] == "Region controller":
+            ansible_user = "hans"
+            group_name = "region_controllers"
+        elif machine['status_name'] == 'Ready':
+            group_name = "ready_machines"
+            ansible_user = "ubuntu"
+        elif machine['status_name'] == 'Deployed':
+            distro = machine.get('distro_series', '').lower()
+            if distro not in os_mapping:
+                continue
+            ansible_user = "ubuntu" if "ubuntu" in os_mapping[distro] else "cloud-user"
+            pool = machine.get('pool', {}).get('name', 'default')
+            group_name = f"{pool}_{os_mapping[distro]}"
+        else:
+            continue
+
+        hostname = machine['fqdn']
+        ip_list = machine.get('ip_addresses', [])
+        ip_address = next((ip for ip in ip_list if ':' not in ip), None)
+
+        if not ip_address:
+            continue
+
+        if group_name not in inventory:
+            inventory[group_name] = {
+                'hosts': []
+            }
+
+        inventory[group_name]['hosts'].append(hostname)
+        inventory['_meta']['hostvars'][hostname] = {
+            'ansible_user': ansible_user,
+            'system_id': machine.get('system_id'),
+            'hostname': machine.get('hostname'),
+            'ansible_ssh_host': ip_address
+        }
+
+    return inventory
+
+def main():
+    if len(sys.argv) == 2 and sys.argv[1] == "--list":
+        all_machines = []
+        for resource in ["regioncontrollers", "rackcontrollers", "machines"]:
+            all_machines += get_machines(resource)
+        inventory = build_inventory(all_machines)
+        print(json.dumps(inventory, indent=2))
+    else:
+        print(json.dumps({}))
+
+if __name__ == "__main__":
+    main()
